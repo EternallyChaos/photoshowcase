21725cb3de658f7fcd9540f6ff6f79340dd190f0
unknown <seirseid73@gmail.com>
2024-02-24 19:07:00 +0530

    fixed logo dimensions

--- src/components/LogoIcon.jsx
+++ src/components/LogoIcon.jsx
@@ -1,18 +1,19 @@
 import Logo from "/public/logo.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 const LogoIcon = () => {
   return (
-    <div>
+    <Link href="/">
       <Image
         alt="logo"
         src={Logo}
         height={0}
         width={0}
         sizes="100vw"
-        className="w-72"
+        className="w-60 lg:w-72"
       />
-    </div>
+    </Link>
   );
 };
 
